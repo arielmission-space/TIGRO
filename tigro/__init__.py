794e084f56a611e4c5c1b5c858dfd8bd029ee57b
andrea.bocchieri <andrea.bocchieri@uniroma1.it>
2024-07-17 17:25:22 +0200

    ported logger from paos

--- tigro/__init__.py
+++ tigro/__init__.py
@@ -11,3 +11,13 @@ __email__ = metadata.metadata("tigro")["Author_email"]
 __license__ = metadata.metadata("tigro")["license"]
 __copyright__ = "2024-{:d}, {}".format(date.today().year, __author__)
 __summary__ = metadata.metadata("tigro")["Summary"]
+
+# initialise logger
+import logging
+from paos.log import setLogLevel
+
+logger = logging.getLogger("paos")
+setLogLevel(logging.DEBUG)
+
+logger.name = __pkg_name__
+logger.info(f"code version {__version__}")
