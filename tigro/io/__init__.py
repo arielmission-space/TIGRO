f3d277bdffeba1007fe5c02c75e1006da54558f4
Enzo Pascale <enzo.pascale@uniroma1.it>
2026-01-25 18:33:10 +0100

    Added capability to read pickles

--- tigro/io/__init__.py
+++ tigro/io/__init__.py
@@ -1,3 +1,4 @@
-from .load import load_phmap, sort_phmap
+from .load import load_phmap, sort_phmap 
+from .get_processed_sequence import get_processed_sequence
 
-__all__ = ["load_phmap", "sort_phmap"]
+__all__ = ["load_phmap", "sort_phmap", "get_processed_sequence"]
