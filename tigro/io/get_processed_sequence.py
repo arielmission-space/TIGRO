20ae23e008e8871c83e4f30b27c02644ec0834c2
andrea.bocchieri <andrea.bocchieri@uniroma1.it>
2026-01-25 20:41:36 +0100

    Refactor get_processed_sequence to support HDF5 files

--- tigro/io/get_processed_sequence.py
+++ tigro/io/get_processed_sequence.py
@@ -1,16 +1,27 @@
 import numpy as np
 import pickle, glob, os
+import h5py
+
+
+def load_recursively_from_h5(group, metadata):
+    for key, item in group.items():
+        if isinstance(item, h5py.Dataset):
+            metadata[key] = item[()]
+        elif isinstance(item, h5py.Group):
+            metadata[key] = {}
+            load_recursively_from_h5(item, metadata[key])
 
 
 def get_processed_sequence(
     sequence,
     path,
     SN=None,
+    ext=".pkl",
 ):
     if SN:
-        full_path = os.path.expanduser(os.path.join(path, "*{:s}*.pkl".format(SN)))
+        full_path = os.path.expanduser(os.path.join(path, "*{:s}*{}".format(SN, ext)))
     else:
-        full_path = os.path.expanduser(os.path.join(path, "*.pkl"))
+        full_path = os.path.expanduser(os.path.join(path, "*{}".format(ext)))
 
     all_files = glob.glob(full_path)
 
@@ -20,7 +31,26 @@ def get_processed_sequence(
         seq = int(seq)
         if seq != sequence:
             continue
-        with open(fname, "rb") as fs:
-            _map, _map_ptt, _map_pttf, _map_residual, _metadata = pickle.load(fs)
-            return _map, _map_ptt, _map_pttf, _map_residual, _metadata
+
+        if ext == ".pkl":
+            with open(fname, "rb") as fs:
+                _map, _map_ptt, _map_pttf, _map_residual, _metadata = pickle.load(fs)
+
+        elif ext == ".h5":
+            with h5py.File(fname, "r") as fs:
+                _data = {}
+                load_recursively_from_h5(fs["data"], _data)
+                _map, _map_ptt, _map_pttf, _map_residual = (
+                    np.ma.masked_invalid(_data["regmap"]),
+                    np.ma.masked_invalid(_data["regmap_ptt"]),
+                    np.ma.masked_invalid(_data["regmap_pttf"]),
+                    np.ma.masked_invalid(_data["regmap_residual"]),
+                )
+                _metadata = {}
+                load_recursively_from_h5(fs["metadata"], _metadata)
+
+        else:
+            raise ValueError("Unsupported file extension: {:s}".format(ext))
+
+        return _map, _map_ptt, _map_pttf, _map_residual, _metadata
     raise FileNotFoundError("Sequence not found.")
